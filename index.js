c9d6c0bad5020cf79e94d4cee5d42a243a27981e
Baltasar Sanchez <baltasar.sanchez@propiedades.com>
2017-01-06 12:07:23 -0600

    use alt media when getting file

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ function checkAuth() {
   }, handleAuthResult);
 };
 
-
-
 function handleAuthResult(authResult) {
   if (authResult && !authResult.error) {
     // Hide auth UI, then load client library.
     $("#authorize-div").modal('hide');
-    loadDriveApi();
+    readFile();
   } else {
     // Show auth UI, allowing the user to initiate authorization by
     // clicking authorize button.
@@ -29,14 +27,6 @@ function handleAuthClick(event) {
   return false;
 }
 
-/**
- * Load Drive API client library.
- */
-function loadDriveApi() {
-  gapi.client.load('drive', 'v3', readFile);
-  console.log('loaded drive');
-};
-
 function readFile() {
   var search_params = window.location.search;
   var state_uri = search_params.replace('?state=', '');
@@ -48,33 +38,13 @@ function readFile() {
     gapi.client.request({
       path: 'https://www.googleapis.com/drive/v3/files/' + file_id,
       params: {
-        fields: 'name,webContentLink'
+        alt: 'media'
       }
     }).then(
       function (resp) {
-        console.log(resp.result);
-        console.log('requesting ' + resp.result.webContentLink);
-      $.ajax(resp.result.webContentLink, { dataType : 'text' }).done(function(xml) {
-        viewer.importXML(xml);
-      });
-        // gapi.client.request({
-          // path: resp.result.webContentLink
-        // }).then(
-          // function (response) {
-            // console.log(response.result);
-          // }, function (err) {console.log(err);}
-        // );
-      },
-      function (err) {console.log(err);}
+        viewer.importXML(resp.body);
+      }
     );
-    // gapi.client.drive.files.get({
-      // fileId: file_id
-    // }).then(
-      // function (resp) {
-        // console.log(resp.result);
-      // },
-      // function (err) {console.log(err);}
-    // );
   } else {
     console.log("unhandled action");
   }
